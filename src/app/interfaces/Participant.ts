ead34dbf347c371c48db96d98411158f899b8cb7
Aaron Falk <aaronfalk25@gmail.com>
2024-08-27 23:00:10 -0400

    minor change to participant interface

--- src/app/interfaces/Participant.ts
+++ src/app/interfaces/Participant.ts
@@ -1,4 +1,13 @@
 export interface Participant {
+    id: string,
+    eventId: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    phoneNumber: string,
+}
+
+export interface ParticipantCreate {
     eventId: string,
     firstName: string,
     lastName: string,
